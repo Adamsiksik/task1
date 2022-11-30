5196403e63772d5e245bfbb0039bb9916c78e31a
Adamsiksik <78469824+Adamsiksik@users.noreply.github.com>
2022-11-30 19:10:53 +0200

    info on the same page

--- src/components/card.js
+++ src/components/card.js
@@ -2,13 +2,13 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { useEffect, useState } from "react";
 
-function CardComp() {
+function CardComp(props) {
   const [stockDetail, setDeatail] = useState();
   const axios = require("axios");
   const getData = async () => {
     await axios
       .get(
-        `https://finnhub.io/api/v1/quote?symbol=${window.location.href.substring(window.location.href.indexOf('=')+1)}&token=ce1m9giad3i9ep8up0f0ce1m9giad3i9ep8up0fg`
+        `https://finnhub.io/api/v1/quote?symbol=${props.name}&token=ce1m9giad3i9ep8up0f0ce1m9giad3i9ep8up0fg`
       )
       .then(function (response) {
         setDeatail(response.data);
@@ -18,9 +18,9 @@ function CardComp() {
       })
       .then(function () {});
   };
+
   useEffect(() => {
     getData();
-    console.log(stockDetail);
   }, []);
 
   return (
@@ -28,7 +28,6 @@ function CardComp() {
       <div>
         {stockDetail ? (
           <Card>
-            <Card.Header>{window.location.href.substring(window.location.href.indexOf('=')+1)}</Card.Header>
             <Card.Body>
               <Card.Title>Price {stockDetail.c}</Card.Title>
               <Card.Text>Opening Price : {stockDetail.o}</Card.Text>
@@ -37,7 +36,7 @@ function CardComp() {
             </Card.Body>
           </Card>
         ) : (
-          <span>Loading Stock...</span>
+          <span>Loading Stock info...</span>
         )}
       </div>
     </>
--- src/pages/stock_detail.js
+++
@@ -1,9 +0,0 @@
-import MainList from "../components/list";
-import React from "react";
-import Card from "../components/card"
-export default function stock_details(props) {
-  console.log(props)
-  return (
-      <Card />
-  );
-}
